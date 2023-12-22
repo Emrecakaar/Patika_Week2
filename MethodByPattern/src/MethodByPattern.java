a76afd70cbec8d54d93dd8d1f1ea26f32a55539f
Emre Cakar <emre_147123@hotmail.com>
2023-12-22 14:31:14 +0100

    Method_By_Pattern

--- MethodByPattern/src/MethodByPattern.java
+++ MethodByPattern/src/MethodByPattern.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class MethodByPattern {
-    static void printPattern(int number, boolean increasing) {
-        System.out.print(number + " ");
-        if (number > 0) {
-            // Artarak devam et
-            printPattern(number - 5, true);
-        } else if (number <= 0){
-            // Azalarak devam et
-            printPattern(number + 5, false);
+    static void printPattern(int number1, int number2, boolean isMethod) {
+        if (number1 > 0 && isMethod) {
+            System.out.print(number1 + " ");
+            printPattern(number1 - 5, number2, true);
+        } else if (number1 <= 0) {
+            System.out.print(number1 + " ");
+            printPattern(number1 + 5, number2, false);
+        } else if (number1 > 0 && number1 <= number2 && !isMethod) {
+            System.out.print(number1 + " ");
+            printPattern(number1 + 5, number2, false);
         }
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Bir sayı giriniz: ");
-        int number = input.nextInt();
+        System.out.print("Bir sayı giriniz : ");
+        int number1 = input.nextInt();
 
-        printPattern(number, true);
+        printPattern(number1, number1, true);
     }
 }
