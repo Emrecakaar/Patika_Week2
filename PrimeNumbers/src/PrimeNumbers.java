3e17ed86147e7e3b900bd956157c67f50926f329
Emre Cakar <emre_147123@hotmail.com>
2023-12-22 12:42:27 +0100

    Prime_Numbers

--- PrimeNumbers/src/PrimeNumbers.java
+++ PrimeNumbers/src/PrimeNumbers.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class PrimeNumbers {
     static boolean primeNumbers(int number, int divisor) {
-        if (divisor <= 1) {
+        if (divisor <= 1) { // Bölen sayımız 1 veya 1 den küçük olursa program dursun istiyorum.
             return true;
         }
-        if (number % divisor == 0) {
+        if (number % divisor == 0) { // Eğer sayı bölünüyorsa false alarak asal sayı olmadığını belirliyorum.
             return false;
         } else {
-            return primeNumbers(number, divisor - 1);
+            return primeNumbers(number, divisor - 1);// sayıyı 1 azaltarak asal sayının kontrolünü sağlıyorum.
         }
     }
 
@@ -18,7 +18,7 @@ public class PrimeNumbers {
         System.out.print("Kontrol etmek istediğiniz sayıyı giriniz : ");
         int number = input.nextInt();
 
-        if (primeNumbers(number, number - 1)) {
+        if (primeNumbers(number, number - 1)) {// Burada da yaptığım işin çıktısını alıyorum.
             System.out.println(number + " sayısı bir asal sayıdır.");
         } else {
             System.out.println(number + " sayısı bir asal sayı değildir.");
