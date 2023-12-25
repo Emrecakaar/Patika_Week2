e7bc0f6159efe1112a4afbdd9e87de3df123589d
Emre Cakar <emre_147123@hotmail.com>
2023-12-25 22:12:50 +0100

    Harmonik

--- Harmonik/src/Main.java
+++ Harmonik/src/Main.java
@@ -1,14 +1,13 @@
-
 public class Main {
     public static void main(String[] args) {
 
-        int[] list = {1, 2, 3, 4, 5};//Array methodu oluşturdum ve değerlerini girdim.
-        double sum = 0.0;// avarage double çıkabilme ihtimali için sum'ı double veri tipi girdim.
-
+        int[] list = {1, 2, 3};//Array methodu oluşturdum ve değerlerini girdim.
+        double sum = 0.0;
         for (int i = 0; i < list.length; i++) {
-            sum += list[i];//for döngüsü ile toplam değerimi buldum
+            sum += 1.0 / list[i];// harmonik serimin bölüm kısmını sum'a eşitleyerek buldum.
+
         }
-        double avarage = sum / list.length;// harmonik sayıma ulaşmak için bu formülü kullandım
-        System.out.println(avarage);//çıktısını verdim.
+        double avarage = (list.length) / sum;// dizideki eleman sayımı sum'a bölerek harmonik serimi oluşturdum.
+        System.out.println(avarage);
     }
 }
