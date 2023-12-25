14690f6523477d82e99c1998da201d9198e8e06a
Emre Cakar <emre_147123@hotmail.com>
2023-12-25 22:50:53 +0100

    Printing_The_Letter_A

--- PrintingTheLetterA/src/Main.java
+++ PrintingTheLetterA/src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         String[][] letter = new String[6][4];
 
-        for (int first = 0; first < letter.length; first++){
-            for (int second = 0; second < letter[first].length; second++ ){
-                if (first == 0 || first == 2){
+        for (int first = 0; first < letter.length; first++){ //İlk önce satırı for döngüsüne bağladım.
+            for (int second = 0; second < letter[first].length; second++ ){//Daha sonra sütunu for döngüsünü bağladım.
+                if (first == 0 || first == 2){//Satırda oluşacak yıldızın yerlerini belirledim.
                     letter[first][second] = " * ";
-                } else if (second == 0 || second == 3){
+                } else if (second == 0 || second == 3){//Sütunda oluşacak yıldızın yerlerini belirledim.
                     letter[first][second] = " * ";
                 } else {
-                    letter[first][second] = "   ";
+                    letter[first][second] = "   ";//Boşluk yerlerini belirledim.
                 }
             }
         }
